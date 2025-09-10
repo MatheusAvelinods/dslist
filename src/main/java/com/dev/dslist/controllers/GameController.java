9942f4c157f1196105c453ed718fb419e6371e3b
Matheus <matheusavelino44@gmail.com>
2025-09-10 15:26:33 -0300

    Domain mode

--- src/main/java/com/dev/dslist/controllers/GameController.java
+++ src/main/java/com/dev/dslist/controllers/GameController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dev.dslist.dto.GameMinDTO;
-import com.dev.dslist.entities.Game;
 import com.dev.dslist.services.GameService;
 
 @RestController
